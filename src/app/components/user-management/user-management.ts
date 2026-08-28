ef0dbd0b9f2714660ba59067a2acf255c67074c3
Andres30211 <felifit27@gmail.com>
2026-08-28 13:46:41 -0500

    Servicio e interfas de localización

--- src/app/components/user-management/user-management.ts
+++ src/app/components/user-management/user-management.ts
@@ -580,7 +580,8 @@ export class UserManagement implements OnInit{
           }
           
         });
-        this.loadUsers();
+        // this.loadUsers();
+        this.dc.detectChanges();
     });
 }
   
